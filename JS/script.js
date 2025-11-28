dfdf453fd424695acc28aec090b9d71ba063e5fa
dacostatimeo <dacostatimeo@gmail.com>
2025-11-28 23:01:12 +0100

    update

--- JS/script.js
+++ JS/script.js
@@ -1,3 +1,16 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const burger = document.querySelector('.burger-menu');
+    const nav = document.querySelector('.nav-links');
+
+    burger.addEventListener('click', () => {
+        // Basculer l'affichage du menu
+        nav.classList.toggle('nav-active');
+        
+        // Animation optionnelle du burger (croix)
+        burger.classList.toggle('toggle');
+    });
+});
+
 document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
 	anchor.addEventListener("click", function (e) {
 		e.preventDefault()
